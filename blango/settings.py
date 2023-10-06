c99a0a55efbe024702bb037aa6d57124a39766eb
Rishabh Dugar <rishabhrishirishu@gmail.com>
2023-10-06 13:50:35 +0000

    Finish django Allauth

--- blango/settings.py
+++ blango/settings.py
@@ -38,9 +38,14 @@ class Dev(Configuration):
     CSRF_COOKIE_SAMESITE = 'None'
     SESSION_COOKIE_SAMESITE = 'None'
     AUTH_USER_MODEL = "blango_auth.User"
+
     ACCOUNT_ACTIVATION_DAYS = 7
     # REGISTRATION_OPEN = False
-    
+    SITE_ID = 1
+    ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+    ACCOUNT_EMAIL_REQUIRED = True
+    ACCOUNT_USERNAME_REQUIRED = False
+    ACCOUNT_AUTHENTICATION_METHOD = "email"
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
     
     PASSWORD_HASHERS = [
@@ -57,6 +62,7 @@ class Dev(Configuration):
         'django.contrib.contenttypes',
         'django.contrib.sessions',
         'django.contrib.messages',
+        "django.contrib.sites",
         'django.contrib.staticfiles',
         
         'crispy_forms',
@@ -65,6 +71,11 @@ class Dev(Configuration):
 
         'blango_auth',
         'blog',
+
+        "allauth", 
+        "allauth.account", 
+        "allauth.socialaccount",
+        "allauth.socialaccount.providers.google",
     ]
 
     MIDDLEWARE = [
